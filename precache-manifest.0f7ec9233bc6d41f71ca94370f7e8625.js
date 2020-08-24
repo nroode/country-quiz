093eb35abc1f86de859823e4129a030d04e053d7
Natalie Roode <nroode@gmail.com>
2020-08-23 17:10:20 -0700

    Updates

---
+++ precache-manifest.0f7ec9233bc6d41f71ca94370f7e8625.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2576a1d4e24b19d975887c89df9713a",
+    "url": "/country-quiz/index.html"
+  },
+  {
+    "revision": "31924370192238a5f25e",
+    "url": "/country-quiz/static/css/main.fae7c862.chunk.css"
+  },
+  {
+    "revision": "04ff3418459f2c089149",
+    "url": "/country-quiz/static/js/2.2a355911.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-quiz/static/js/2.2a355911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31924370192238a5f25e",
+    "url": "/country-quiz/static/js/main.95bfb481.chunk.js"
+  },
+  {
+    "revision": "6732ad9d2e8a21389367",
+    "url": "/country-quiz/static/js/runtime-main.a24aa1a9.js"
+  },
+  {
+    "revision": "7e2f750c3fa6f8e7240e017f2fae0113",
+    "url": "/country-quiz/static/media/background.7e2f750c.png"
+  },
+  {
+    "revision": "eae25da4c02c031beef455d31ee43749",
+    "url": "/country-quiz/static/media/undraw_adventure.eae25da4.svg"
+  },
+  {
+    "revision": "82d9605afcc8a7a321691203895e1aa3",
+    "url": "/country-quiz/static/media/undraw_winners.82d9605a.svg"
+  }
+]);
